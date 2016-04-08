5e92af75b6d5a9681689b9699825c6dbac66e9c2
yangning-xxnr <yangning@xinxinnongren.com>
2016-04-08 18:58:48 +0800

    4.8

--- xinnongreniOS/Classes/appMain/AppDelegate.m
+++ xinnongreniOS/Classes/appMain/AppDelegate.m
@@ -104,14 +104,14 @@
         [[NSUserDefaults standardUserDefaults] synchronize];
     }
     
-    BugtagsOptions *options = [[BugtagsOptions alloc] init];
-    options.trackingCrashes = YES;        // 是否收集闪退，联机 Debug 状态下默认 NO，其它情况默认 YES
-    options.trackingUserSteps = YES;      // 是否跟踪用户操作步骤，默认 YES
-    options.trackingConsoleLog = YES;     // 是否收集控制台日志，默认 YES
-    options.trackingUserLocation = YES;   // 是否获取位置，默认 YES
-    options.trackingNetwork = YES;        // 是否跟踪网络请求，默认 NO
-    [Bugtags startWithAppKey:@"a059d969fd904e985d25a480b071f8cf" invocationEvent:BTGInvocationEventBubble options:options];
-    [Bugtags startWithAppKey:@"a059d969fd904e985d25a480b071f8cf" invocationEvent:BTGInvocationEventBubble];
+//    BugtagsOptions *options = [[BugtagsOptions alloc] init];
+//    options.trackingCrashes = YES;        // 是否收集闪退，联机 Debug 状态下默认 NO，其它情况默认 YES
+//    options.trackingUserSteps = YES;      // 是否跟踪用户操作步骤，默认 YES
+//    options.trackingConsoleLog = YES;     // 是否收集控制台日志，默认 YES
+//    options.trackingUserLocation = YES;   // 是否获取位置，默认 YES
+//    options.trackingNetwork = YES;        // 是否跟踪网络请求，默认 NO
+//    [Bugtags startWithAppKey:@"a059d969fd904e985d25a480b071f8cf" invocationEvent:BTGInvocationEventBubble options:options];
+//    [Bugtags startWithAppKey:@"a059d969fd904e985d25a480b071f8cf" invocationEvent:BTGInvocationEventBubble];
     return YES;
 }
 
