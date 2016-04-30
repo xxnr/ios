2f69d4201a9e1c9a9df1ac23ac2f7f887c49d876
xxnr-chung <zhongzhenhua@xinxinnongren.com>
2016-04-30 08:12:45 +0800

    gitIgnore

--- xinnongreniOS/Classes/Mine/Controller/XNRRscOrderDetialController.m
+++ xinnongreniOS/Classes/Mine/Controller/XNRRscOrderDetialController.m
@@ -12,7 +12,35 @@
 
 -(void)viewDidLoad
 {
+    [super viewDidLoad];
+    [self setNavigationBar];
+    self.view.backgroundColor = R_G_B_16(0xffffff);
     
 }
+
+-(void)setNavigationBar
+{
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0 , 100, 44)];
+    titleLabel.backgroundColor = [UIColor clearColor];
+    titleLabel.font = [UIFont systemFontOfSize:20];
+    titleLabel.textColor = [UIColor colorWithRed:256.0/256.0 green:256.0/256.0 blue:256.0/256.0 alpha:1.0];//设置文本颜色
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.text = @"服务站订单";
+    self.navigationItem.titleView = titleLabel;
+    
+    UIButton *backButton=[UIButton buttonWithType:UIButtonTypeCustom];
+    backButton.frame = CGRectMake(0, 0, 80, 44);
+    backButton.imageEdgeInsets = UIEdgeInsetsMake(0, -60, 0, 0);
+    [backButton addTarget:self action:@selector(backClick) forControlEvents:UIControlEventTouchUpInside];
+    [backButton setImage:[UIImage imageNamed:@"top_back.png"] forState:UIControlStateNormal];
+    UIBarButtonItem *leftItem =[[UIBarButtonItem alloc]initWithCustomView:backButton];
+    self.navigationItem.leftBarButtonItem=leftItem;
+}
+
+-(void)backClick
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 
 @end
